18c21191bc14a1c5ed10f9339b0df5a0bb9a42c9
cokeby190 <cokeby190@gmail.com>
2012-06-05 16:59:22 +0800

    Test commit NUS Foodie

---
+++ src/com/android/dev/foodie/NUSFoodieActivity.java
@@ -0,0 +1,32 @@
+package com.android.dev.foodie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class NUSFoodieActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button searchButton = (Button)findViewById(R.id.search);
+
+        searchButton.setOnClickListener(new OnClickListener() {
+        	
+		@Override
+		public void onClick(View view) {
+			Intent open_search = new Intent(NUSFoodieActivity.this, search_act.class);
+			startActivity(open_search);
+		}
+
+        });
+        
+        
+        
+    }
+}
---
+++ src/com/android/dev/foodie/search_act.java
@@ -0,0 +1,13 @@
+package com.android.dev.foodie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class search_act extends Activity{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_page);
+        
+    }
+}
